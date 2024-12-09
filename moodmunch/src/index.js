0750ad5cd639e173d1c1056ff1bfec81f8c047b4
emmy-aa <emmyapaloo12@gmail.com>
2024-12-09 06:03:33 -0500

    Added documentation comments to describe index.js file.

--- moodmunch/src/index.js
+++ moodmunch/src/index.js
@@ -1,3 +1,11 @@
+/**
+ * @file index.js
+ * @description This file is the root entry point for the React application.
+ * It initializes the React DOM and renders the main App component inside the root HTML element.
+ * React Strict Mode is enabled to highlight potential issues in the application.
+ */
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
