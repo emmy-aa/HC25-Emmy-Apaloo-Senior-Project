faf08c588923bf9b7196989d46876bb0f8d994c1
emmy-aa <emmyapaloo12@gmail.com>
2024-12-09 03:10:10 -0500

    Cleaned up code and added a filterByCategory section as well as a dropdown feature for ambiance

--- moodmunch/src/components/Home.js
+++ moodmunch/src/components/Home.js
@@ -1,68 +1,90 @@
 import React, { useState } from 'react'
 import Fooddata from './data'
 import Card from 'react-bootstrap/Card'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const Home = () => {
 
     const [menu, setMenu] = useState(Fooddata);
+    const [ambianceFilter, setAmbianceFilter] = useState('All')
 
-    const filteritems = (curitems) => {
-        const updateitems = Fooddata.filter((cur) => {
-            return cur.category === curitems;
-        })
-        setMenu(updateitems);
-    };
-    const filterAmbiance = (type) => {
-        const updateitems = Fooddata.filter((cur) => cur.ambiance === type);
-        setMenu(updateitems);
-    };
+    const filterByCategory = (category) => {
+        if (category === 'All') {
+            setMenu(Fooddata)
+        }else{
+            const updatedItems = Fooddata.filter((cur)=>cur.category === category)
+            setMenu(updatedItems);
+        }
 
+    };
+    const filterByAmbiance = (ambiance) => {
+        setAmbianceFilter(ambiance);
+        if (ambiance === "All") {
+            setMenu(Fooddata);
+        } else {
+            // Assume `ambiance` property is added to Fooddata
+            const updatedItems = Fooddata.filter((cur) => cur.ambiance === ambiance);
+            setMenu(updatedItems);
+        }
+    };
+    const sortBy = (type) => {
+        const sortedMenu = [...menu].sort((a, b) => {
+            if (type === "rating") return b.rating - a.rating;
+            if (type === "price") return parseFloat(a.price.slice(1)) - parseFloat(b.price.slice(1));
+        });
+        setMenu(sortedMenu);
+    };
 
     return (
         <>
-            <section className='iteam_section mt-2 container'>
-                <h2 className='text-center mb-2' style={{fontWeight: 400}}>MoodMunch</h2>
-
-                <div className="btn-container d-flex justify-content-around mt-2">
-                    <button className="btn btn-primary" onClick={() => filteritems("Indian")}>Indian</button>
-                    <button className="btn btn-success" onClick={() => filteritems("Italian")}>Italian</button>
-                    <button className="btn btn-danger" onClick={() => filteritems("American")}>American</button>
-                    <button className="btn btn-primary" onClick={() => filteritems("Chinese")}>Chinese</button>
-                    <button className="btn btn-primary" onClick={() => setMenu(Fooddata)}>All</button>
+            <section className='filter_section container mt-4'>
+                <h2 className='text-center mb-3' style={{ fontWeight: 400 }}>MoodMunch</h2>
+                <div className="btn-container d-flex justify-content-around">
+                    <button className="btn btn-primary" onClick={() => filterByCategory("Indian")}>Indian</button>
+                    <button className="btn btn-success" onClick={() => filterByCategory("Italian")}>Italian</button>
+                    <button className="btn btn-danger" onClick={() => filterByCategory("American")}>American</button>
+                    <button className="btn btn-secondary" onClick={() => setMenu(Fooddata)}>All</button>
                 </div>
-
-                <div className="btn-container d-flex justify-content-around mt-2">
-                    <button className="btn btn-warning" onClick={() => filterAmbiance("Cozy")}>Cozy</button>
-                    <button className="btn btn-info" onClick={() => filterAmbiance("Happy")}>Happy</button>
-                    <button className="btn btn-secondary" onClick={() => filterAmbiance("Upscale")}>Upscale</button>
-                    <button className="btn btn-primary" onClick={() => setMenu(Fooddata)}>All</button>
+                <Dropdown className="mt-3">
+                    <Dropdown.Toggle variant="info" id="dropdown-basic">
+                        Filter by Ambiance
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => filterByAmbiance("All")}>All</Dropdown.Item>
+                        <Dropdown.Item onClick={() => filterByAmbiance("Cozy")}>Cozy</Dropdown.Item>
+                        <Dropdown.Item onClick={() => filterByAmbiance("Happy")}>Happy</Dropdown.Item>
+                        <Dropdown.Item onClick={() => filterByAmbiance("Upscale")}>Upscale</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <div className="sort-buttons mt-3 d-flex justify-content-center">
+                    <button className="btn btn-warning mx-2" onClick={() => sortBy("rating")}>Sort by Rating</button>
+                    <button className="btn btn-warning mx-2" onClick={() => sortBy("price")}>Sort by Price</button>
                 </div>
+            </section>
 
-
-                <div className='container mt-3'>
+            <section className='item_section mt-3 container'>
+                <div className='container'>
                     <div className="row d-flex justify-content-center align-items-center">
                         {menu.map((e) => {
                             return (
-                                <>
-                                    <Card key={e.id} style={{width: '22rem', border: "none"}}
-                                          className="mx-2 mt-4 card_style">
-                                        <Card.Img variant="top" src={e.imgdata} style={{height: "16rem"}}
-                                                  className='mt-3'/>
-                                        <Card.Body>
-                                            <Card.Title>{e.rname}</Card.Title>
-                                            <Card.Text>
-                                                Price : {e.price}
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                </>
-                            )
+                                <Card key={e.id} style={{ width: '22rem', border: "none" }} className="mx-2 mt-4 card_style">
+                                    <Card.Img variant="top" src={e.imgdata} style={{ height: "16rem" }} className='mt-3' />
+                                    <Card.Body>
+                                        <Card.Title>{e.rname}</Card.Title>
+                                        <Card.Text>
+                                            Price: {e.price} | Rating: {e.rating}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            );
                         })}
                     </div>
                 </div>
             </section>
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
+
+
