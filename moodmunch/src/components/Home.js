e24fba2a4ee326a883f8dc9cc17ef1476f69ea71
emmy-aa <emmyapaloo12@gmail.com>
2024-12-09 01:21:41 -0500

    Updated the Home component

--- moodmunch/src/components/Home.js
+++ moodmunch/src/components/Home.js
@@ -1,4 +1,4 @@
-mport React from 'react'
+import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
