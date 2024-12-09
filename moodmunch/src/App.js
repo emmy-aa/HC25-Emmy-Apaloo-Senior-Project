4e6969c783942d5d8192ab748c956a39ff2e47a6
emmy-aa <emmyapaloo12@gmail.com>
2024-12-09 06:01:42 -0500

    Added documentation comments to describe App.js file.

--- moodmunch/src/App.js
+++ moodmunch/src/App.js
@@ -1,3 +1,11 @@
+/**
+ * @file App.js
+ * @description This is the main entry point for the React application.
+ * It imports and renders the Header and Home components, as well as global styles.
+ * The file sets up the overall structure and layout of the application.
+ */
+
+
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
